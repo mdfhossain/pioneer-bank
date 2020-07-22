0657a6498b4f7d280ba719b2316d21a80f9716c2
Md. Faisal Hossain <mdfhossain@ael-bd.com>
2020-07-23 02:58:17 +0600

    Added bank deposit and total balance

---
+++ js/script.js
@@ -0,0 +1,47 @@
+//Login Event Handler
+const loginBtn = document.getElementById('btnLogin');
+loginBtn.addEventListener('click', function () {
+    //console.log('Click Login Button');
+    const loginArea = document.getElementById('loginArea');
+    loginArea.style.display = 'none';
+
+    const dashboardArea = document.getElementById('dashboardArea');
+    dashboardArea.style.display = 'flex';
+});
+
+
+//Deposit Event Handler
+const depositBtn = document.getElementById('btnDeposit');
+depositBtn.addEventListener('click', function () {
+    const depositNumber = getAmount('depositAmount');
+
+    updateBalance('currentDeposit', depositNumber);
+    updateBalance('currentTotalBalance', depositNumber);
+
+    document.getElementById('depositAmount').value = '';
+});
+
+
+//Withdraw Event Handler
+const withDrawBtn = document.getElementById('btnWithDraw');
+withDrawBtn.addEventListener('click', function () {
+    const withDrawNumber = getAmount('withDrawAmount');
+});
+
+
+
+function getAmount(id) {
+    const amount = document.getElementById(id).value;
+    const withDrawNumber = parseFloat(amount);
+
+    return withDrawNumber;
+}
+
+
+function updateBalance(id, depositNumber) {
+    const current = document.getElementById(id).innerText;
+    const currentNumber = parseFloat(current);
+    const totalAmount = depositNumber + currentNumber;
+    document.getElementById(id).innerText = totalAmount;
+}
+
